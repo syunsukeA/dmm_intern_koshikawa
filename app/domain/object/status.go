b38e93d2ee7d2df01e7d57e7d5a9d7eb237d0e3b
koshikawa <19rd067@ms.dendai.ac.jp>
2023-08-03 15:19:29 +0900

    add db tag to Object.Status structure

--- app/domain/object/status.go
+++ app/domain/object/status.go
@@ -9,16 +9,16 @@ import (
 
 type Status struct {
 	// The internal ID of the status
-	ID int64 `json:"id,omitempty"`
+	ID int64 `json:"id,omitempty"  db:"id"`
 
 	// The internal ID of the account
-	AccountID int64 `json:"account_id,omitempty"`
+	AccountID int64 `json:"account_id,omitempty" db:"account_id"`
 
 	// The content of the status
-	Content string `json:"content,omitempty"`
+	Content string `json:"content,omitempty" db:"content"`
 
 	// URL to the ???
-	URL *string `json:"url,omitempty"`
+	URL *string `json:"url,omitempty" db:"url"`
 
 	// The time the status was created
 	CreateAt time.Time `json:"create_at,omitempty" db:"create_at"`
