b87811d55893f86f67803a3c1a70075b2860d0a6
koshikawa <19rd067@ms.dendai.ac.jp>
2023-08-03 14:59:09 +0900

    add deffinition of status table

--- ddl/ddl.sql
+++ ddl/ddl.sql
@@ -9,3 +9,12 @@ CREATE TABLE `account` (
   `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `status` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `account_id` bigint(20) NOT NULL,
+  `content` varchar(255) NOT NULL, -- ToDo: contentの上限決めないとまずそう？text型に変更したい
+  `url` text,
+  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
